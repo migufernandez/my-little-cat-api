0cc25ec441914fc1c69723342a723e4ae4869613
Miguel <mfernandezsaenz@gmail.com>
2017-10-04 10:44:31 -0400

    added getting started. Added routing

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # my-little-cat-api
+
+## Introduction
+
+## Getting started
+
+Welcome to the internal API team. As a developer on the team (not a public developer) use the instructions below to clone the repo, install dependencies and start the API.
+
+```
+
+$ git clone https://github.com/migufernandez/my-little-cat-api.git
+$ cd my-little-cat-api
+$ npm install
+$ node start
+
+```
---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+
+const cats = [
+  { id: 'tabby', type: 'breed', desc: 'a great cat' },
+  { id: 'siamese', type: 'breed', desc: 'a bad cat' },
+  { id: 'maine coon', type: 'breed', desc: 'a fat cat' },
+  { id: 'minx', type: 'breed', desc: 'a crazy cat' },
+  {
+    id: 'felix',
+    type: 'cat',
+    name: 'felix',
+    breed: 'tabby',
+    owner: 'miguel',
+    age: 10
+  },
+  {
+    id: 'hector',
+    type: 'cat',
+    name: 'hector',
+    breed: 'siamese',
+    owner: 'cam',
+    age: 12
+  },
+  {
+    id: 'juan',
+    type: 'cat',
+    name: 'juan',
+    breed: 'maine coon',
+    owner: 'bill',
+    age: 14
+  },
+  {
+    id: 'mike',
+    type: 'cat',
+    name: 'mike',
+    breed: 'minx',
+    owner: 'curry',
+    age: 16
+  }
+]
+
+app.get('/', function(req, res) {
+  res.send('Welcome to the cat api, meow.')
+})
+
+app.get('/breeds', function(req, res) {
+  res.send('You hit the /breeds route. Coming soon.')
+})
+
+app.get('/breeds/:id', function(req, res) {
+  const breedID = req.params.id
+  res.send(
+    'You hit the /breeds/:id route. Coming soon. The breed id is: ' + breedID
+  )
+})
+
+app.get('/cats', function(req, res) {
+  res.send('You hit the /cats route. Coming soon.')
+})
+
+app.get('/cats/:id', function(req, res) {
+  const catID = req.params.id
+  res.send('You hit the /cats/:id route. Coming soon. The cat id is: ' + catID)
+})
+
+app.listen(4000, () => console.log('API is UP! MEOW! on port 4000'))
